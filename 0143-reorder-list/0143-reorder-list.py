3a1da966f2d60f580ee7b8477fa56c018a4b998c
Sachin Ramachandran <sachin.ramachandran2006@gmail.com>
2026-08-28 18:52:37 +0530

    Time: 6 ms (74.49%), Space: 30 MB (96.24%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def reorderList(self, head):
+        slow = head
+        fast = head.next
+
+        # Find middle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Split list
+        second = slow.next
+        slow.next = None
+
+        
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+       
+        first = head
+        second = prev
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
